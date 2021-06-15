9c8ca17a734e1a4a10b9454d2dda3e9c6e384c40
Ishmam Ali <ishmamalikhan@gmail.com>
2021-06-15 22:46:19 +0500

    online store

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Banner from './components/Banner';
+import ProductsContextProvider from './Global/ProductsContext';
+import Products from './components/Products';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Cart from './components/Cart';
+import Error from './components/Error';
+import CartContextProvider from './Global/CartContext';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  return (
+    <div>
+      <ProductsContextProvider>
+      <CartContextProvider>
+      <Router>
+      <Navbar/>
+        <Switch>
+        <Route path="/" exact component={Products}/>
+        <Route path="/cart" exact component={Cart}/>
+        <Route path="*" exact component={Error}/>
+        </Switch>
+      </Router>
+      </CartContextProvider>
+      </ProductsContextProvider>
+    </div>
+  );
+}
+
+export default App;
